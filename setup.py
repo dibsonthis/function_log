69ad879f4f0ff62bdb7ea94ec43aa1206f2567c2
crhuffer <crhuffer@gmail.com>
2021-03-27 20:01:25 -0400

    Changed folder structure. And updated setup.py and setup.cfg.

--- setup.py
+++ setup.py
@@ -3,12 +3,8 @@ from setuptools import setup
 setup(name='function_log',
       version='0.1',
       description='Logging and timing tests for Python functions',
-      url='https://github.com/dibsonthis/function_log',
-      author='Adib Attie',
-      author_email='adib.attie33@gmail.com',
+      url='https://github.com/chuffer/function_log',
       license='MIT',
       packages=['function_log'],
-      install_requires=[
-
-      ],
+      package_dir = {'': 'src'},
       zip_safe=False)
